2a0592e4b11862832e43d62c9134f3b79e4d39ad
Bantaaa <114312925+Bantaaa@users.noreply.github.com>
2024-02-17 23:28:18 +0100

    added notifaction button with its counter on the navbar

--- resources/views/includes/layout.blade.php
+++ resources/views/includes/layout.blade.php
@@ -115,14 +115,40 @@
     @endif
   <!--  -->
     @if(session('Fname'))
-        <span class="pr-3">{{ session('Fname') }}</span>
-        <a class="inline-block no-underline hover:text-black" href="#">
-            <svg class="fill-current hover:text-black" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                <circle fill="none" cx="12" cy="7" r="3" />
-                <path d="M12 2C9.243 2 7 4.243 7 7s2.243 5 5 5 5-2.243 5-5S14.757 2 12 2zM12 10c-1.654 0-3-1.346-3-3s1.346-3 3-3 3 1.346 3 3S13.654 10 12 10zM21 21v-1c0-3.859-3.141-7-7-7h-4c-3.86 0-7 3.141-7 7v1h2v-1c0-2.757 2.243-5 5-5h4c2.757 0 5 2.243 5 5v1H21z" />
-            </svg>
-        </a>
-        @endif
+    <span class="pr-3">{{ session('Fname') }}</span>
+            <div class="relative inline-block">
+                <button id="notification-toggle" class="flex items-center focus:outline-none">
+                    <svg class="fill-current hover:text-black" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                    <path d="M19.3399 14.49L18.3399 12.83C18.1299 12.46 17.9399 11.76 17.9399 11.35V8.82C17.9399 6.47 16.5599 4.44 14.5699 3.49C14.0499 2.57 13.0899 2 11.9899 2C10.8999 2 9.91994 2.59 9.39994 3.52C7.44994 4.49 6.09994 6.5 6.09994 8.82V11.35C6.09994 11.76 5.90994 12.46 5.69994 12.82L4.68994 14.49C4.28994 15.16 4.19994 15.9 4.44994 16.58C4.68994 17.25 5.25994 17.77 5.99994 18.02C7.93994 18.68 9.97994 19 12.0199 19C14.0599 19 16.0999 18.68 18.0399 18.03C18.7399 17.8 19.2799 17.27 19.5399 16.58C19.7999 15.89 19.7299 15.13 19.3399 14.49Z" fill="#292D32"/>
+<path d="M14.8297 20.01C14.4097 21.17 13.2997 22 11.9997 22C11.2097 22 10.4297 21.68 9.87969 21.11C9.55969 20.81 9.31969 20.41 9.17969 20C9.30969 20.02 9.43969 20.03 9.57969 20.05C9.80969 20.08 10.0497 20.11 10.2897 20.13C10.8597 20.18 11.4397 20.21 12.0197 20.21C12.5897 20.21 13.1597 20.18 13.7197 20.13C13.9297 20.11 14.1397 20.1 14.3397 20.07C14.4997 20.05 14.6597 20.03 14.8297 20.01Z" fill="#292D32"/>
+                    </svg>
+                    <div class="absolute top-0 right-0 -mt-2 -mr-2 bg-red-500 text-white rounded-full w-4 h-4 flex items-center justify-center text-xs">5</div>
+                </button>
+                <div id="notification-dropdown" class="absolute right-0 mt-2 py-2 w-48 bg-white rounded-md shadow-xl hidden">
+                    <!-- Dropdown content goes here -->
+                    <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-gray-200">Notification 1</a>
+                    <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-gray-200">Notification 2</a>
+                    <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-gray-200">Notification 3</a>
+                </div>
+            </div>
+            @endif
+            <script>
+    document.addEventListener("DOMContentLoaded", function() {
+        var notificationToggle = document.getElementById("notification-toggle");
+        var notificationDropdown = document.getElementById("notification-dropdown");
+
+        notificationToggle.addEventListener("click", function() {
+            notificationDropdown.classList.toggle("hidden");
+        });
+
+        document.addEventListener("click", function(event) {
+            if (!notificationToggle.contains(event.target) && !notificationDropdown.contains(event.target)) {
+                notificationDropdown.classList.add("hidden");
+            }
+        });
+    });
+</script>
+            <!--  -->
     <!--  -->
 </div>
 @if(session('Fname'))
